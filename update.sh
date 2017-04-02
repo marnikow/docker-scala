6c3436d36440c20dc3b5f55d73bba1e7cf724b63
Cesar Perez <cperez@wnohang.com>
2017-04-02 22:34:00 +0200

    Added a workaround to allow Scala images to be used in Wercker's internal/docker-push steps

--- update.sh
+++ update.sh
@@ -11,30 +11,7 @@ declare -a versions=(
 for version in "${versions[@]}"
 do
   rm -rf "$version"
-  mkdir "$version"
 
-  cat > "$version/Dockerfile" <<-END
-# Generated automatically by update.sh
-# Do no edit this file
-
-FROM openjdk:8-alpine
-
-# The bash shell is required by Scala utilities
-RUN apk add --no-cache bash
-
-# Install build dependencies
-RUN apk add --no-cache --virtual=.dependencies tar
-
-RUN wget -O- "http://downloads.lightbend.com/scala/$version/scala-$version.tgz" \\
-    |  tar xzf - -C /usr/local --strip-components=1
-
-# Remove build dependencies
-RUN apk del --no-cache .dependencies
-
-VOLUME /app
-WORKDIR /app
-
-CMD ["scala"]
-END
-
+  cp -R base "$version"
+  sed -e "s/<version>/$version/g" Dockerfile.template > "$version/Dockerfile"
 done
