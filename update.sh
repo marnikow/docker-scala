deae462a4b914b076c75151743bfbe7a14ab98c4
Cesar Perez <cperez@wnohang.com>
2017-10-10 10:38:05 +0200

    Added the standard image generation mechanishm

--- update.sh
+++ update.sh
@@ -3,24 +3,25 @@
 set -eo pipefail
 
 declare -a versions=(
-  2.12.3
-  2.12.2
-  2.12.1
-  2.12.0
-  2.11.11
-  2.11.10
-  2.11.9
-  2.11.8
-  2.10.6
-  2.10.5
-  2.10.4
-  2.10.3
+    2.12.0
+    2.11.11
+    2.10.6
 )
+
+generate_standard() {
+  sed -e "s/<version>/$1/g" dockerfile/standard.template > "$1/Dockerfile"
+}
+
+generate_alpine() {
+  cp -R base/alpine "$1/alpine"
+  sed -e "s/<version>/$1/g" dockerfile/alpine.template > "$1/alpine/Dockerfile"
+}
 
 for version in "${versions[@]}"
 do
-  rm -rf "$version"
-
-  cp -R base "$version"
-  sed -e "s/<version>/$version/g" Dockerfile.template > "$version/Dockerfile"
+  rm -rf "${version}"
+  mkdir "${version}"
+  
+  generate_standard "${version}"
+  generate_alpine "${version}"
 done
